d6c3ce6318ff9beb9e28b74efc521e1ced7f3ee1
Hamed Fakoori <hamedfakoori78@gmail.com>
2024-03-22 15:26:36 +0330

    feat: use postgresql for default database add database image to docker-compose file.

--- backend/configs/settings.py
+++ backend/configs/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'configs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('DB_USER'),
+        'PASSWORD': env.str('DB_PASSWORD'),
+        'HOST': env.str('DB_HOST'),
+        'PORT': env.int('DB_PORT'),
     }
 }
 
