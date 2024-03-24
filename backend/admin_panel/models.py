e6af28860de2fa9f7f4d47321385a1baabcf568e
Hamed Fakoori <hamedfakoori78@gmail.com>
2024-03-24 18:39:07 +0330

    chore: create product model in admin_panel.models

--- backend/admin_panel/models.py
+++ backend/admin_panel/models.py
@@ -10,3 +10,15 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    slug = models.SlugField(unique=True , max_length=128)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.IntegerField(default=1)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
