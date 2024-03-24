2b719926914becd92233fffefb1837044357de07
Hamed Fakoori <hamedfakoori78@gmail.com>
2024-03-24 20:37:10 +0330

    chore: add shipping address model to django admin panel

--- backend/admin_panel/admin.py
+++ backend/admin_panel/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from admin_panel.models import Category , Product , ProductImage
+from admin_panel.models import Category , Product , ProductImage , ShippingAddress
 
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(ProductImage)
+admin.site.register(ShippingAddress)
