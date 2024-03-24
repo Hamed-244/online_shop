0c6e57b03060220f9591ce54155133c9c48acda4
Hamed Fakoori <hamedfakoori78@gmail.com>
2024-03-24 20:34:44 +0330

    chore: add product image to django admin panel

--- backend/admin_panel/admin.py
+++ backend/admin_panel/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from admin_panel.models import Category , Product
+from admin_panel.models import Category , Product , ProductImage
 
 
 admin.site.register(Category)
 admin.site.register(Product)
+admin.site.register(ProductImage)
