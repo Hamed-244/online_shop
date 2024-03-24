acd40781fde727999f58631bd0187af65ff71ab1
Hamed Fakoori <hamedfakoori78@gmail.com>
2024-03-24 20:57:24 +0330

    chore: register order model in django admin panel

--- backend/admin_panel/admin.py
+++ backend/admin_panel/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from admin_panel.models import Category , Product , ProductImage , ShippingAddress
+from admin_panel.models import Category , Product , ProductImage , ShippingAddress \
+    , Order
 
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(ProductImage)
 admin.site.register(ShippingAddress)
+admin.site.register(Order)
