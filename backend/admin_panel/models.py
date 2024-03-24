c26ab548854224a3ceaa8da3724bcf55d3735f4f
Hamed Fakoori <hamedfakoori78@gmail.com>
2024-03-24 18:33:46 +0330

    chore : create category model in admin.models

--- backend/admin_panel/models.py
+++ backend/admin_panel/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+    description = models.CharField(max_length=255)
+    parent_category = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name="parent")
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
