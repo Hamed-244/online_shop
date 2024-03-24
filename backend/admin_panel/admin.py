0ff42ca7cc14da890e483afc2322096336b4775e
Hamed Fakoori <hamedfakoori78@gmail.com>
2024-03-24 18:40:49 +0330

    chore: register product model for django admin panel

--- backend/admin_panel/admin.py
+++ backend/admin_panel/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from admin_panel.models import Category
+from admin_panel.models import Category , Product
 
 
 admin.site.register(Category)
+admin.site.register(Product)
