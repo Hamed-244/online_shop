4a7c0e9fc7a3242c8f1f2b0e748b4cc17142e056
Hamed Fakoori <hamedfakoori78@gmail.com>
2024-03-24 20:08:07 +0330

    chore: create product image model to add image to products

--- backend/admin_panel/models.py
+++ backend/admin_panel/models.py
@@ -22,3 +22,12 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='products/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.name
