8b28aae1b9a89765d0f4f0e0d83153aa226b9a88
Hamed Fakoori <hamedfakoori78@gmail.com>
2024-04-01 13:34:00 +0330

    refactor: refactor pagination class to use django filter and ra-data-django-rest-framework settings

--- backend/admin_panel/pagination.py
+++ backend/admin_panel/pagination.py
@@ -2,20 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 class AdminPanelPagination(PageNumberPagination):
-    def get_paginated_response(self, data):
-        return Response(data)
-    
     page_size = 10
-    page_size_query_param = 'perPage'
+    page_size_query_param = 'page_size'
     max_page_size = 50
-
-    def get_paginated_response(self, data):
-        total_items = self.page.paginator.count
-        item_starting_index = self.page.start_index() - 1 # In a page, indexing starts from 1
-        item_ending_index = self.page.end_index() - 1
-
-        content_range = 'items {0}-{1}/{2}'.format(item_starting_index, item_ending_index, total_items)      
-
-        headers = {'Content-Range': content_range} 
-
-        return Response(data, headers=headers)
